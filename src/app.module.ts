3ee517aa545dbf11934757f892b9dfe4cbee23a9
Leonardo Souza <leoss91@hotmail.com>
2023-05-19 17:13:49 -0300

    update auth0 config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { HttpModule } from './http/http.module';
+import { TestController } from './test/test.controller';
 
 @Module({
   imports: [DatabaseModule, HttpModule],
