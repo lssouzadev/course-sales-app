6dea3e57d3d1ba3c9602241e21dbd92d383e0f29
Leonardo Da Silva Souza <leoss91@hotmail.com>
2023-05-20 12:24:45 -0300

    set configuration to docker/prisma

---
+++ purchases/prisma/migrations/20230520145359_create_customers_table/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "Costumer" (
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "Costumer_pkey" PRIMARY KEY ("id")
+);
--- purchases/src/app.module.ts
+++ purchases/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { HttpModule } from './http/http.module';
-import { TestController } from './test/test.controller';
 
 @Module({
   imports: [DatabaseModule, HttpModule],
